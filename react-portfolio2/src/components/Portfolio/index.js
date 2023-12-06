ea66488bf4947d363e1b7045cd2926fde2da46f1
Daley Jones <djones_15@hotmail.com>
2023-12-06 20:38:25 +0000

    added new img

--- react-portfolio2/src/components/Portfolio/index.js
+++ react-portfolio2/src/components/Portfolio/index.js
@@ -5,6 +5,7 @@ import AnimatedLetters from "../AnimatedLetters";
 import techImage from '../../assets/images/tech.png';
 import quizImage from '../../assets/images/quizz.png';
 import gitImage from '../../assets/images/project 2.gif';
+import pacImage from '../../assets/images/pacman game.gif';
 import holidayImage from '../../assets/images/holidayexplorer.png';
 
 import "./index.scss";
@@ -32,34 +33,37 @@ const Portfolio = () => {
 
   const getGithubProjects = async () => {
     const projectsData = [
-    
-            {
-              name: 'Git Jobs',
-              description: 'Git-Jobs is a specialized recruitment platform exclusively tailored for software developers. This platform empowers users to effortlessly browse, bookmark, and submit job applications..',
-              image: gitImage,
-              html_url: 'https://github.com/JackStockwell/git-jobs.git',
-            },
-            {
-              name: 'Holiday Explorer',
-              description: 'Holiday Explorer is a web application that helps users discover and explore various holiday destinations around the world. The application utilizes HTML5, CSS, JavaScript (including libraries like jQuery), and server-side APIs such as the Google Maps API and OpenWeatherMap API.',
-              image: holidayImage,
-              html_url: 'https://github.com/daleyjones/project-1-holidayexplorer.git',
-            },
-            {
-              name: 'The Tech Blog',
-              description: 'This web application allows developers to publish their tech-related articles, blog posts, and thoughts while also enabling them to engage with and comment on posts created by other developers. This project follows the Model-View-Controller (MVC) architectural pattern and utilizes various technologies to create a robust content management system (CMS).',
-              image: techImage,
-              html_url: 'https://github.com/daleyjones/project-1-holidayexplorer.git',
-            },
-            {
-              name: 'JavaScript Quiz',
-              description: 'This is a simple quiz application built with HTML, CSS, and JavaScript. It features multiple choice questions with a timer and the ability to save scores.',
-              image: quizImage,
-              html_url: 'https://github.com/daleyjones/quiz-week4-javascript-code.git',
-            },
-          ];
-          
-
+        {
+            name: 'Pacman Javascript Game',
+            description: 'I am currently working on a Pacman game using JavaScript. ',
+          image: pacImage,
+            html_url: 'https://github.com/daleyjones',
+          },
+      {
+        name: 'Git Jobs',
+        description: 'Git-Jobs is a specialized recruitment platform exclusively tailored for software developers. This platform empowers users to effortlessly browse, bookmark, and submit job applications..',
+        image: gitImage,
+        html_url: 'https://github.com/JackStockwell/git-jobs.git',
+      },
+      {
+        name: 'Holiday Explorer',
+        description: 'Holiday Explorer is a web application that helps users discover and explore various holiday destinations around the world. The application utilizes HTML5, CSS, JavaScript (including libraries like jQuery), and server-side APIs such as the Google Maps API and OpenWeatherMap API.',
+        image: holidayImage,
+        html_url: 'https://github.com/daleyjones/project-1-holidayexplorer.git',
+      },
+      {
+        name: 'The Tech Blog',
+        description: 'This web application allows developers to publish their tech-related articles, blog posts, and thoughts while also enabling them to engage with and comment on posts created by other developers. This project follows the Model-View-Controller (MVC) architectural pattern and utilizes various technologies to create a robust content management system (CMS).',
+        image: techImage,
+        html_url: 'https://github.com/daleyjones/project-1-holidayexplorer.git',
+      },
+      {
+        name: 'JavaScript Quiz',
+        description: 'This is a simple quiz application built with HTML, CSS, and JavaScript. It features multiple choice questions with a timer and the ability to save scores.',
+        image: quizImage,
+        html_url: 'https://github.com/daleyjones/quiz-week4-javascript-code.git',
+      },
+    ];
 
     setGithubProjects(projectsData);
   }
@@ -89,17 +93,17 @@ const Portfolio = () => {
 
   const renderGithubProjects = () => {
     return (
-        <div className="github-projects">
+      <div className="github-projects">
         {githubProjects.map((project, idx) => (
-          <div className="github-project" key={idx}>
+          <div className="github-project" key={idx} onClick={() => openProjectDetails(project)}>
             <h2>{project.name}</h2>
             <p>{project.description}</p>
             <img
-  style={{ maxWidth: '100%', maxHeight: '300px', objectFit: 'contain' }}
-  className="github-project-image"
-  src={project.image}
-  alt="Project"
-/>
+              style={{ maxWidth: '100%', maxHeight: '300px', objectFit: 'contain' }}
+              className="github-project-image"
+              src={project.image}
+              alt="Project"
+            />
 
             <a href={project.html_url} target="_blank" rel="noopener noreferrer">
               View on GitHub
@@ -107,13 +111,13 @@ const Portfolio = () => {
           </div>
         ))}
       </div>
-      
     );
   }
-  
+
   return (
     <>
       <div className="container portfolio-page">
+        {/* Portfolio Section */}
         <h1 className="page-title">
           <AnimatedLetters
             letterClass={letterClass}
