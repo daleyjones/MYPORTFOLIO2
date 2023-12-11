008a5e891fd3ed36eae1698e429ac986322a64a2
Daley Jones <djones_15@hotmail.com>
2023-12-11 16:44:09 +0000

    fixed test errors

--- react-portfolio2/src/index.js
+++ react-portfolio2/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import 'matchmedia-polyfill';
+
 import App from './App';
 import './index.css';
 
--- react-portfolio2/src/setupTests.js
+++ react-portfolio2/src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+// setupTests.js
+
+// Mock matchMedia to fix the 'matchMedia not present' error
+window.matchMedia = window.matchMedia || function() {
+    return {
+      matches: false,
+      addListener: function() {},
+      removeListener: function() {}
+    };
+  };
+  
